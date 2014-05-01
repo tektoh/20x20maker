05b09d2710b803428769154ea4dc0079118de693
Shinichi Segawa <tektoh@gmail.com>
2014-05-02 01:15:46 +0900

    provision時に不要な上書きをしないように修正

--- script.sh
+++ script.sh
@@ -10,10 +10,16 @@ else
 fi
 
 # SETUP CAKEPHP CONFIG
-cp app/Config/core.php.sample app/Config/core.php
-cp app/Config/database.php.sample app/Config/database.php
+if [ ! -f app/Config/core.php ]; then
+  cp -v app/Config/core.php.sample app/Config/core.php
+  cp -v app/Config/database.php.sample app/Config/database.php
+else
+  echo SKIP: SETUP CAKEPHP CONFIG
+fi
 
 # CREATE DATABASE
 if [ ! -f app/tmp/database/default ]; then
   yes | php app/Console/cake.php -app /vagrant/app schema create App
+else
+  echo SKIP: CREATE DATABASE
 fi
